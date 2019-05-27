fc35cb50f5df80502d35bcdbf1488c5f2750926a
xiangyanzhai <770935856@qq.com>
2019-05-27 09:14:02 +0800

    YOLOv3

---
+++ test/YOLOv3_coco_test.py
@@ -0,0 +1,279 @@
+# !/usr/bin/python
+# -*- coding:utf-8 -*-
+import os
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+import sys
+
+sys.path.append('../../')
+import numpy as np
+import tensorflow as tf
+
+import tensorflow.contrib.slim as slim
+import YOLOv3.tool.yolo_utils as yolo_utils
+from YOLOv3.tool.config import Config
+from YOLOv3.tool.yolo_predict import predict
+
+from datetime import datetime
+
+base_anchors = [[10, 13], [16, 30], [33, 23], [30, 61], [62, 45], [59, 119], [116, 90], [156, 198], [373, 326]]
+base_anchors = tf.constant(base_anchors, dtype=tf.float32)
+base_anchors = tf.reshape(base_anchors, (3, 3, 2))
+base_anchors = base_anchors[::-1]
+
+
+# base_anchors = np.array(base_anchors, np.float32)
+# base_anchors = base_anchors.reshape(3, 3, 2)
+# base_anchors = base_anchors[::-1]
+
+
+def get_coord(N, stride):
+    print(N, stride)
+    t = tf.range(int(N / stride))
+    x, y = tf.meshgrid(t, t)
+
+    x = x[..., None]
+    y = y[..., None]
+    coord = tf.concat((x, y, x, y), axis=-1)
+    coord = coord[:, :, None, :]
+    coord = tf.to_float(coord) * stride
+    return coord
+
+
+def loc2bboxes(net, anchors, coord, stride):
+    xy = tf.nn.sigmoid(net[..., :2]) * stride
+    wh = tf.exp(net[..., 2:4]) * anchors
+    xy1 = xy - wh / 2
+    xy2 = xy + wh / 2
+    bboxes = tf.concat((xy1, xy2), axis=-1) + coord
+    return bboxes
+
+
+def decode_net(net, anchors, coord, stride):
+    xy = tf.nn.sigmoid(net[..., :2]) * stride
+    wh = tf.exp(net[..., 2:4]) * anchors
+    xy1 = xy - wh / 2
+    xy2 = xy + wh / 2
+    bboxes = tf.concat((xy1, xy2), axis=-1) + coord
+    net = tf.sigmoid(net[..., 4:])
+    return tf.concat([bboxes, net], axis=-1)
+
+
+def loadNumpyAnnotations(data):
+    """
+    Convert result data from a numpy array [Nx7] where each row contains {imageID,x1,y1,w,h,score,class}
+    :param  data (numpy.ndarray)
+    :return: annotations (python nested list)
+    """
+    print('Converting ndarray to lists...')
+    assert (type(data) == np.ndarray)
+    print(data.shape)
+    assert (data.shape[1] == 7)
+    N = data.shape[0]
+    ann = []
+    for i in range(N):
+        if i % 1000000 == 0:
+            print('{}/{}'.format(i, N))
+
+        ann += [{
+            'image_id': int(data[i, 0]),
+            'bbox': [data[i, 1], data[i, 2], data[i, 3], data[i, 4]],
+            'score': data[i, 5],
+            'category_id': int(data[i, 6]),
+        }]
+
+    return ann
+
+
+class YOLOv3():
+
+    def __init__(self, config):
+        self.config = config
+        self.stride = config.stride
+        self.blocks = yolo_utils.parse_cfg(config.cfg_file)
+        self.coords = []
+        for i in range(len(self.stride)):
+            self.coords.append(get_coord(config.img_size, self.stride[i]))
+
+    def build_net(self):
+        size = self.config.img_size
+        self.im_input = tf.placeholder(dtype=tf.string)
+        # self.im_input = tf.placeholder(dtype=tf.float32,shape=[None,None,3])
+        im, self.b_scale, self.im_h, self.im_w = yolo_utils.handle_im_same_wh(self.im_input, size=size)
+        m = tf.shape(im)[0]
+        with slim.arg_scope([slim.batch_norm], is_training=False, scale=True, decay=0.9, epsilon=1e-5):
+            with slim.arg_scope([slim.conv2d], weights_regularizer=slim.l2_regularizer(self.config.weight_decay)):
+                Name, end_points = yolo_utils.create_net(im, self.blocks)
+        self.Name = Name
+
+        for i in range(len(Name)):
+            print(i, '\t', Name[i], '\t', end_points[i])
+
+        net13 = end_points[82]
+        net26 = end_points[94]
+        net52 = end_points[-1]
+        map_H = tf.shape(net13)[1]
+        self.map_H = map_H
+        C = [net13, net26, net52]
+
+        decode = []
+        for i in range(3):
+            tH = map_H * 2 ** i
+            decode.append(tf.reshape(decode_net(tf.reshape(C[i], (-1, tH, tH, 3, self.config.num_cls + 5)),
+                                                base_anchors[i], self.coords[i][:tH, :tH], self.stride[i]),
+                                     (m, -1, self.config.num_cls + 5)))
+        decode = tf.concat(decode, axis=1)
+        self.result = predict(decode[0], size, self.config.num_cls, c_thresh=0.005, iou_thresh_=0.45)
+
+    def init(self, sess, Name, init_vars, W):
+        # W = yolo_utils.load_weights(self.config.weight_file, self.blocks, Name, init_vars, (80 + 5) * 5, count=5)
+        print('***********', len(W), len(init_vars))
+        for i in range(len(W)):
+            weight = W[i]
+            if init_vars[i].get_shape().as_list()[-1] != (self.config.num_cls + 5) * 3:
+                print(i, init_vars[i].name, init_vars[i].shape, weight.shape)
+                sess.run(init_vars[i].assign(weight))
+            else:
+                sess.run(init_vars[i].assign(weight))
+                print('*************************')
+        print('init finish')
+        pass
+
+    def test_coco(self):
+        self.build_net()
+        for v in tf.global_variables():
+            print(v)
+        catId2cls, cls2catId, catId2name = joblib.load(
+            '/home/zhai/PycharmProjects/Demo35/Demo/(catId2cls,cls2catId,catId2name).pkl')
+        test_dir = r'/home/zhai/PycharmProjects/Demo35/dataset/coco/val2017/'
+        names = os.listdir(test_dir)
+        names = [name.split('.')[0] for name in names]
+        names = sorted(names)
+
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        W = yolo_utils.load_weights(self.config.weight_file, self.blocks, self.Name, tf.global_variables(),
+                                    (80 + 5) * 3, count=5)
+        joblib.dump(W, 'yolov3_coco.pkl')
+        print(len(W), len(tf.global_variables()))
+        with tf.Session(config=config) as sess:
+            self.init(sess, self.Name, tf.global_variables(), W)
+            Res = []
+            i = 0
+            mm = 10
+            time_start = datetime.now()
+            for name in names[:mm]:
+                i += 1
+                print(datetime.now(), i)
+                im_file = test_dir + name + '.jpg'
+                img = tf.gfile.FastGFile(im_file, 'rb').read()
+                # img=cv2.imread(im_file)[...,::-1]
+                res, box_scale, h, w = sess.run([self.result, self.b_scale, self.im_h, self.im_w],
+                                                feed_dict={self.im_input: img})
+                res = res[..., :200]
+                res[:, :4] = res[:, :4] * box_scale
+                x1 = res[:, [0]]
+                y1 = res[:, [1]]
+                x2 = res[:, [2]]
+                y2 = res[:, [3]]
+                x1 = np.clip(x1, 0, w - 1)
+                y1 = np.clip(y1, 0, h - 1)
+                x2 = np.clip(x2, 0, w - 1)
+                y2 = np.clip(y2, 0, h - 1)
+                res[:, :4] = np.concatenate((x1, y1, x2, y2), axis=-1)
+                wh = res[:, 2:4] - res[:, :2] + 1
+
+                imgId = int(name)
+                m = res.shape[0]
+
+                imgIds = np.zeros((m, 1)) + imgId
+
+                cls = res[:, 5]
+                cid = map(lambda x: cls2catId[x], cls)
+                cid = list(cid)
+                cid = np.array(cid)
+                cid = cid.reshape(-1, 1)
+
+                res = np.concatenate((imgIds, res[:, :2], wh, res[:, 4:5], cid), axis=1)
+                # Res=np.concatenate([Res,res])
+                res = np.round(res, 4)
+                Res.append(res)
+
+            Res = np.concatenate(Res, axis=0)
+            Ann = loadNumpyAnnotations(Res)
+            print('==================================', mm, datetime.now() - time_start)
+
+            with codecs.open('yolov3_bbox.json', 'w', 'ascii') as f:
+                json.dump(Ann, f)
+            eval_coco_box.eval('yolov3_bbox.json', mm)
+
+    # def test(self):
+    #     self.build_net()
+    #
+    #     file = '/home/zhai/PycharmProjects/Demo35/yolo/yolo_last_m_gpu/models/yolov3_voc_train_74_last_same_dist_m_gpu.ckpt-999'
+    #     # file = '/home/zhai/PycharmProjects/Demo35/yolo/yolo_last/models/yolov3_voc_train_74_last_same_dist.ckpt-4999'
+    #     file = '/home/zhai/PycharmProjects/Demo35/yolo/yolov3_last/models/yolov3_voc_4.ckpt-15000'
+    #
+    #     saver = tf.train.Saver()
+    #     test_dir = r'/home/zhai/PycharmProjects/Demo35/dataset/voc/VOCtest2007/VOCdevkit/VOC2007/JPEGImages/'
+    #     names = os.listdir(test_dir)
+    #     names = [name.split('.')[0] for name in names]
+    #     names = sorted(names)
+    #
+    #     config = tf.ConfigProto()
+    #     config.gpu_options.allow_growth = True
+    #     with tf.Session() as sess:
+    #         saver.restore(sess, file)
+    #         Res = {}
+    #         i = 0
+    #         m = 100
+    #         time_start = datetime.now()
+    #         for name in names[:m]:
+    #             i += 1
+    #             print(datetime.now(), i)
+    #             im_file = test_dir + name + '.jpg'
+    #             img = tf.gfile.FastGFile(im_file, 'rb').read()
+    #             # img = cv2.imread(im_file)
+    #             res, box_scale, h, w = sess.run([self.result, self.b_scale, self.im_h, self.im_w],
+    #                                             feed_dict={self.im_input: img})
+    #             # res = res[:200]
+    #             res[:, :4] = res[:, :4] * box_scale
+    #             x1 = res[:, [0]]
+    #             y1 = res[:, [1]]
+    #             x2 = res[:, [2]]
+    #             y2 = res[:, [3]]
+    #             x1 = np.clip(x1, 0, w - 1)
+    #             y1 = np.clip(y1, 0, h - 1)
+    #             x2 = np.clip(x2, 0, w - 1)
+    #             y2 = np.clip(y2, 0, h - 1)
+    #             res[:, :4] = np.concatenate((x1, y1, x2, y2), axis=-1)
+    #             Res[name] = res
+    #
+    #         print(datetime.now() - time_start)
+    #         joblib.dump(Res, 'yolov3_4.pkl')
+    #     import cv_Map
+    #     cv_Map.eval(Res, m, True)
+    #     cv_Map.eval(Res, m, False)
+    #     pass
+
+    pass
+
+
+from sklearn.externals import joblib
+import cv2
+import codecs
+import json
+import eval_coco_box
+
+if __name__ == "__main__":
+    Mean = np.array([123.68, 116.78, 103.94], dtype=np.float32)
+    path = '/home/zhai/PycharmProjects/Demo35/data_set_yxyx/'
+    files = [path + 'voc_07.tf', path + 'voc_12.tf']
+    cfgfile = '../cfg/yolov3.cfg'
+    weightsfile = '/home/zhai/PycharmProjects/Demo35/yolo/yolo_weights/yolov3.weights'
+
+    config = Config(files, cfgfile, weightsfile, num_cls=80, img_size=544, ignore_thresh=0.5)
+
+    yolov3 = YOLOv3(config)
+    yolov3.test_coco()
+    pass
---
+++ train/YOLOv3_train.py
@@ -0,0 +1,249 @@
+# !/usr/bin/python
+# -*- coding:utf-8 -*-
+import os
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+import sys
+
+sys.path.append('../../')
+import numpy as np
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import YOLOv3.tool.yolo_utils as yolo_utils
+from YOLOv3.tool.config import Config
+from YOLOv3.tool.read_Data import readData
+from YOLOv3.tool.yolov3_loss import yolov3_loss
+from datetime import datetime
+
+base_anchors = [[10, 13], [16, 30], [33, 23], [30, 61], [62, 45], [59, 119], [116, 90], [156, 198], [373, 326]]
+base_anchors = tf.constant(base_anchors, dtype=tf.float32)
+base_anchors = tf.reshape(base_anchors, (3, 3, 2))
+base_anchors = base_anchors[::-1]
+
+
+def average_gradients(tower_grads):
+    average_grads = []
+    for grad_and_vars in zip(*tower_grads):
+        grads = [tf.expand_dims(g, 0) for g, _ in grad_and_vars]
+        grads = tf.concat(grads, 0)
+        grad = tf.reduce_mean(grads, 0)
+        grad_and_var = (grad, grad_and_vars[0][1])
+        # [(grad0, var0),(grad1, var1),...]
+        average_grads.append(grad_and_var)
+    return average_grads
+
+
+def get_coord(N, stride):
+    t = tf.range(int(N / stride))
+    x, y = tf.meshgrid(t, t)
+
+    x = x[..., None]
+    y = y[..., None]
+    coord = tf.concat((x, y, x, y), axis=-1)
+    coord = coord[:, :, None, :]
+    coord = tf.to_float(coord) * stride
+    return coord
+
+
+def loc2bboxes(net, anchors, coord, stride):
+    xy = tf.sigmoid(net[..., :2]) * stride
+
+    wh = tf.exp(net[..., 2:4]) * anchors
+    xy1 = xy - wh / 2
+    xy2 = xy + wh / 2
+    bboxes = tf.concat((xy1, xy2), axis=-1) + coord
+    return bboxes
+
+
+class YOLOv3():
+
+    def __init__(self, config):
+        self.config = config
+        self.stride = config.stride
+        self.blocks = yolo_utils.parse_cfg(config.cfg_file)
+        self.coords = []
+        for i in range(len(self.stride)):
+            self.coords.append(get_coord(config.img_size, self.stride[i]))
+
+    def init(self, sess, Name, init_vars):
+        W = yolo_utils.load_weights(self.config.weight_file, self.blocks, Name, init_vars, (80 + 5) * 3, count=5)
+        print('***********', len(W), len(init_vars))
+        for i in range(len(W)):
+            weight = W[i]
+            if init_vars[i].get_shape().as_list()[-1] != (self.config.num_cls + 5) * 3:
+                print(i, init_vars[i].name, init_vars[i].shape, weight.shape)
+                sess.run(init_vars[i].assign(weight))
+            else:
+                print('*************************')
+        print('init finish')
+        pass
+
+    def fn_map(self, x):
+        C = x[0]
+        pre_bboxes = x[1]
+        bboxes = x[2][:x[3]]
+        loss = yolov3_loss(C, pre_bboxes, bboxes, self.map_H, self.size, self.config.num_cls, self.config.ignore_thresh)
+        return loss
+
+    def get_loss(self, C, pre_bboxes, bboxes, nums, ):
+        loss = tf.map_fn(self.fn_map, [C, pre_bboxes, bboxes, nums], tf.float32)
+        return loss
+
+    def build_net(self, Iter, size):
+
+        self.size = size
+        imgs, bboxes, nums = Iter.get_next()
+
+        imgs = tf.image.resize_images(imgs, (size, size))
+        scale = tf.to_float(size) / self.config.read_img_size
+        m = tf.shape(imgs)[0]
+        bboxes = tf.concat([bboxes[..., :4] * scale, bboxes[..., 4:]], axis=-1)
+        # param_regularizers = {'gamma': slim.l2_regularizer(self.config.weight_decay)}
+        with slim.arg_scope([slim.batch_norm], is_training=True, scale=True, decay=0.9, epsilon=1e-5,
+                            ):
+            with slim.arg_scope([slim.conv2d], weights_regularizer=slim.l2_regularizer(self.config.weight_decay)):
+                Name, end_points = yolo_utils.create_net(imgs / 255, self.blocks)
+        self.Name = Name
+        init_vars = tf.global_variables()[2:]
+        for i in range(len(Name)):
+            print(i, '\t', Name[i], '\t', end_points[i])
+
+        net13 = end_points[82]
+        net26 = end_points[94]
+        net52 = end_points[-1]
+
+        del end_points
+        # for i in range(len(end_points)-1):
+        #     if i not in [82,94]:
+        #         del end_points[i]
+        map_H = tf.shape(net13)[1]
+        self.map_H = map_H
+        C = [net13, net26, net52]
+
+        tcoords = []
+        pre_bboxes = []
+        for i in range(3):
+            tH = map_H * 2 ** i
+            tcoords.append(self.coords[i][:tH, :tH])
+            C[i] = tf.reshape(C[i], (-1, tH, tH, 3, self.config.num_cls + 5))
+
+            pre_bboxes.append(tf.reshape(loc2bboxes(C[i], base_anchors[i], tcoords[i], self.stride[i]), (m, -1, 4)))
+        C = list(map(lambda c: tf.reshape(c, (m, -1, self.config.num_cls + 5)), C))
+        C = tf.concat(C, axis=1)
+        pre_bboxes = tf.concat(pre_bboxes, axis=1)
+        loss = self.get_loss(C, pre_bboxes, bboxes, nums)
+        return tf.reduce_sum(loss) / tf.to_float(m), init_vars
+
+    def train(self):
+        shard_nums = self.config.gpus
+        batch_size = self.config.gpus * self.config.batch_size_per_GPU
+        print('******************* batch_size', batch_size)
+        print('*******************  shard_nums', shard_nums)
+        base_lr = self.config.lr
+        print('******************* base_lr', base_lr)
+        steps = tf.Variable(0.0, name='yolo', trainable=False)
+        x = 1
+        lr = tf.case({steps < 40000.0 * x: lambda: base_lr,
+                      steps < 50000.0 * x: lambda: base_lr / 10},
+                     default=lambda: base_lr / 100)
+
+        tower_grads = []
+        opt = tf.train.MomentumOptimizer(lr, 0.9)
+        var_reuse = False
+        Iter_list = []
+
+        c = 0
+        size_num = self.config.size_num
+        Size = np.arange(size_num, dtype=np.int32) * 32 + 320
+        Size = tf.constant(Size)
+        size_inds = tf.Variable(0, dtype=tf.int32, trainable=False)
+        tt = tf.cond(tf.equal(steps % 10, 0), lambda: tf.random_uniform(shape=(), maxval=size_num, dtype=tf.int32),
+                     lambda: size_inds)
+
+        size_inds = size_inds.assign(tt)
+        size = Size[size_inds]
+
+        for i in range(shard_nums):
+            with tf.device('/gpu:%d' % i):
+                print('************ shard_index', c)
+                Iter = readData(self.config.files, self.config, batch_size=self.config.batch_size_per_GPU,
+                                num_threads=16,
+                                shuffle_buffer=1024,
+                                num_shards=shard_nums, shard_index=c)
+                Iter_list.append(Iter)
+                with tf.variable_scope('', reuse=var_reuse):
+                    if c == 0:
+                        loss, init_vars = self.build_net(Iter, size)
+
+                    else:
+                        loss, _ = self.build_net(Iter, size)
+
+                    var_reuse = True
+
+                c += 1
+
+                train_vars = tf.trainable_variables()
+                l2_loss = tf.losses.get_regularization_losses()
+                l2_re_loss = tf.add_n(l2_loss)
+                yolov3_loss = loss + l2_re_loss
+                grads_and_vars = opt.compute_gradients(yolov3_loss, train_vars)
+                tower_grads.append(grads_and_vars)
+
+        for v in tf.global_variables():
+            print(v)
+
+        grads = average_gradients(tower_grads)
+        grads = list(zip(*grads))[0]
+
+        grads, norm = tf.clip_by_global_norm(grads, 100.0)
+
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            train_op = opt.apply_gradients(zip(grads, train_vars), global_step=steps)
+        saver = tf.train.Saver(max_to_keep=200)
+        config = tf.ConfigProto()
+        # config.gpu_options.allow_growth = True
+        config.allow_soft_placement = True
+
+        # file = '/home/zhai/PycharmProjects/Demo35/YOLOv3/train/models/yolov3_voc_1.ckpt-25000'
+        with tf.Session(config=config) as sess:
+            sess.run(tf.global_variables_initializer())
+            self.init(sess, self.Name, init_vars)
+
+            # saver.restore(sess, file)
+
+            for Iter in Iter_list:
+                sess.run(Iter.initializer)
+
+            for i in range(00000, 50200 * x):
+                if i % 20 == 0:
+
+                    _, yolov3_loss_, loss_, l2_re_loss_, size_, norm_, lr_ = sess.run(
+                        [train_op, yolov3_loss, loss, l2_re_loss, size, norm, lr])
+
+                    print(datetime.now(), 'yolov3_loss:%.4f' % yolov3_loss_, 'loss:%.4f' % loss_,
+                          'l2_re_loss:%.4f' % l2_re_loss_, 'norm:%.4f' % norm_, i, size_, lr_)
+
+                else:
+                    sess.run(train_op)
+
+                if (i + 1) % 5000 == 0 or ((i + 1) % 1000 == 0 and i < 10000):
+                    saver.save(sess, os.path.join('./models/', 'yolov3_voc_1.ckpt'), global_step=i + 1)
+            saver.save(sess, os.path.join('./models/', 'yolov3_voc_1.ckpt'), global_step=i + 1)
+
+    pass
+
+
+if __name__ == "__main__":
+    Mean = np.array([123.68, 116.78, 103.94], dtype=np.float32)
+    path = '/home/zhai/PycharmProjects/Demo35/data_set_yxyx/'
+    files = [path + 'voc_07.tf', path + 'voc_12.tf']
+    cfgfile = '/home/zhai/PycharmProjects/Demo35/yolo/cfg/yolov3-voc.cfg'
+    weightsfile = '/home/zhai/PycharmProjects/Demo35/yolo/yolo_weights/darknet53.conv.74'
+
+    config = Config(files, cfgfile, weightsfile, gpus=1, batch_size_per_GPU=16, lr=0.001 / 2, img_size=608,
+                    read_img_size=416, size_num=8,
+                    ignore_thresh=0.5, crop_iou=0.45, keep_ratio=1 / 5., jitter_ratio=[0.3, 0.5, 0.7])
+    yolov3 = YOLOv3(config)
+    yolov3.train()
+    pass
